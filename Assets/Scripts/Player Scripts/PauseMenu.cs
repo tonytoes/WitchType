278a7fe62c8cd3d37c175ea73fa2fc2ae3879da6
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-09-05 17:01:08 +0800

    pause

---
+++ Assets/Scripts/Player Scripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+//  [小鈎ハレ]  //
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject ButtonPause;
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Escape))
+    //     {
+    //         if (GameIsPaused)
+    //         {
+    //             Resume();
+    //         }
+    //         else
+    //         {
+    //             Pause();
+    //         }
+    //     }
+    // }
+
+    public void ButtonClickPause()
+    {
+
+        if (GameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        ButtonPause.SetActive(true);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        ButtonPause.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Home");
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
