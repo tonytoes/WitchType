d8b247fbdc051f3987a0e29cef6b69f121dd06d2
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-09-07 00:29:49 +0800

    3.0.3
    added scene transitions

---
+++ Assets/Scripts/UI/SceneTransition.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneTransition : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void Play()
+    {
+        StartCoroutine(Transition(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator Transition(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+    
+}
