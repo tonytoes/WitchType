b65a149f963c99592a398e06c7e97243e67b46ca
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-09-19 17:23:07 +0800

    5.1.1
    tab button for spell book ui

--- Assets/Scripts/UI/SpellBookUI.cs
+++ Assets/Scripts/UI/SpellBookUI.cs
@@ -6,6 +6,22 @@ public class SpellBookUI : MonoBehaviour
     public TMP_Text counterText;
     public GameObject spellBookPanel; 
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!spellBookPanel.activeSelf)
+            {
+                OpenSpellBook();
+            }
+            else
+            {
+                CloseSpellBook();
+            }
+        }
+    }
+
+
     private void Start()
     {
         spellBookPanel.SetActive(false);
