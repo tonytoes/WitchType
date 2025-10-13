f3f3512b67b6c4a0a94eb955a4fbb1c1c5381676
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-10-13 23:29:56 +0800

    book collected popup

--- Assets/Scripts/Player/PlayerSpellCollector.cs
+++ Assets/Scripts/Player/PlayerSpellCollector.cs
@@ -6,20 +6,32 @@ public class PlayerSpellCollector : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.CompareTag("Player"))
         {
             var spellManager = SpellManager.Instance;
             if (spellManager == null)
             {
+                Debug.LogWarning("SpellManager instance not found!");
                 return;
             }
 
+    
             if (spellIndex >= 0 && spellIndex < spellManager.allSpells.Count)
             {
                 var spellUnlock = spellManager.allSpells[spellIndex];
                 spellManager.UnlockSpell(spellUnlock);
+
+                
+                if (CollectPopUpUI.instance != null)
+                {
+                    CollectPopUpUI.instance.ShowCollectPopup();
+                }
+                else
+                {
+                    Debug.LogWarning("CollectPopUpUI instance not found in scene!");
+                }
             }
+
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/UI/CollectPopUpUI.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+
+public class CollectPopUpUI : MonoBehaviour
+{
+    public static CollectPopUpUI instance;
+
+    [Header("Popup Settings")]
+    public GameObject collectPopupPrefab;
+    public Transform popupParent;
+    public float popupDuration = 1f;
+    public string triggerTag = "Spell";
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void ShowCollectPopup()
+    {
+        if (collectPopupPrefab == null || popupParent == null)
+        {
+            Debug.LogWarning("CollectPopUpUI: Missing prefab or popupParent reference!");
+            return;
+        }
+
+        GameObject popupInstance = Instantiate(collectPopupPrefab, popupParent);
+        Animator popupAnimator = popupInstance.GetComponent<Animator>();
+
+        
+
+        StartCoroutine(HidePopupAfterDelay(popupInstance));
+    }
+
+    private IEnumerator HidePopupAfterDelay(GameObject popup)
+    {
+        yield return new WaitForSeconds(popupDuration);
+        Destroy(popup);
+    }
+}
+
+
+
+
+
+//  public void AddCollectible()
+//     {
+//         if (isGameOver) return;  // ignore if already ended
+//         count++;
+//         SetCountText();
+//         // create a new popup each time
+//         GameObject popupInstance = Instantiate(collectpopup, popupParent.transform);
+//         audioManager.PlaySFX(audioManager.collect);
+
+//         StartCoroutine(HidePopupAfterDelay(popupInstance));
+//     }
+
+//     private IEnumerator HidePopupAfterDelay(GameObject popup)
+//     {
+//         yield return new WaitForSeconds(1f);
+//         Destroy(popup);
+//     }
