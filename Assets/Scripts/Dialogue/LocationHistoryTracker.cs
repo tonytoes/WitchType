3ef11b634a1ac2c2b96dd967960c0ac87c56d9d8
tonytoes <anthonymiguel2005@gmail.com>
2025-10-20 22:22:05 +0800

    Trackers

---
+++ Assets/Scripts/Dialogue/LocationHistoryTracker.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationHistoryTracker : MonoBehaviour
+{
+    public static LocationHistoryTracker Instance;
+    private List<LocationSO> locationsVisited = new List<LocationSO>();
+
+    public void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public void RecordLocation(LocationSO locationSO)
+    {
+        locationsVisited.Add(locationSO);
+        Debug.Log("Just Visted " + locationSO.displayName);
+    }
+
+    public bool HasVisited(LocationSO locationSO)
+    {
+        return locationsVisited.Contains(locationSO);
+    }
+}
---
+++ Assets/Scripts/Dialogue/SO/LocationSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LocationSO")]
+public class LocationSO : ScriptableObject
+{
+    public string locationID;
+    public string displayName;
+
+}
