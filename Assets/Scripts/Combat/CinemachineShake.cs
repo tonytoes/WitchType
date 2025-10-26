ca27747be186122158b8be2328e4b69d7472631c
tonytoes <anthonymiguel2005@gmail.com>
2025-10-27 01:06:05 +0800

    a

--- Assets/Scripts/Combat/CinemachineShake.cs
+++ Assets/Scripts/Combat/CinemachineShake.cs
@@ -5,16 +5,21 @@ public class CinemachineShake : MonoBehaviour
 {
     public static CinemachineShake Instance;
     private CinemachineImpulseSource _impulseSource;
+    [SerializeField] private float shakeCooldown = 0.3f;
+    private float lastShakeTime;
 
     private void Awake()
     {
         Instance = this;
         _impulseSource = GetComponent<CinemachineImpulseSource>();
     }
-
-    public void Shake(float intensity = 0.5f)
+    public void ShakeOnce(float intensity = 0.5f)
     {
+        if (Time.time - lastShakeTime < shakeCooldown)
+            return;
         _impulseSource.GenerateImpulse(intensity);
+        lastShakeTime = Time.time;
+
     }
 
 }
